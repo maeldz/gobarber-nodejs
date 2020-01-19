f3e6c3f71d97d05556fdb84ab9804bc8a0e602f4
Tomb Raider Evolution <mael.dazareia@gmail.com>
2020-01-19 19:02:47 -0300

    chore: add dotenv

--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,11 @@
+require("dotenv/config");
+
 module.exports = {
-    dialect: "mysql",
-    host: "127.0.0.1",
-    username: "root",
-    password: "root",
-    database: "gonodemodulo2",
+    dialect: process.env.DB_DIALECT,
+    host: process.env.DB_HOST,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
     operatorAliases: false,
     define: {
         timestamp: true,
